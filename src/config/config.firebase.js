1c5cc81c3683444444814ef3e8a772929b92ee54
Khanh Do <khanhdo2909@gmail.com>
2021-01-18 19:48:55 +0700

    update git ignore

---
+++ src/config/config.firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4ptK0k01C0TaubWQQWgW3Bu-hBD5GH9g",
+  authDomain: "cijs-d06.firebaseapp.com",
+  projectId: "cijs-d06",
+  storageBucket: "cijs-d06.appspot.com",
+  messagingSenderId: "995592965052",
+  appId: "1:995592965052:web:4ecd3e63bd0d3c0a6939b9",
+};
---
+++ src/ultis/ulti.js
@@ -0,0 +1,5 @@
+import { firebaseConfig } from "../config/config.firebase";
+
+function showInfo() {
+  console.log(firebaseConfig);
+}
