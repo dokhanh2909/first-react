41d6899306570b769d19fb878cef473d8f6b6d1a
Khanh Do <khanhdo2909@gmail.com>
2021-01-22 18:59:47 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { showInfo } from "./ultis/ulti";
+import { showInfo, signUp } from "./ultis/ulti";
 
 function App() {
+  showInfo();
+  signUp({ username: "linh6", password: "12345" });
   return <div> Hello World </div>;
 }
 
--- src/ultis/ulti.js
+++ src/ultis/ulti.js
@@ -1,5 +1,69 @@
+import "firebase";
+import "firebase/firestore";
+import firebase from "firebase/app";
 import { firebaseConfig } from "../config/config.firebase";
 
-function showInfo() {
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (e) {
+  console.log("Firebase app was installed");
+}
+
+const db = firebase.firestore();
+
+export function showInfo() {
   console.log(firebaseConfig);
+}
+
+// export function signUp(userInformation) {
+//   return new Promise((resolve, reject) => {
+//     const { username, password } = userInformation;
+//     console.log(userInformation);
+//     db.collection("user")
+//       .where("username", "==", username)
+//       .get()
+//       .then((querySnapshot) => {
+//         if (querySnapshot.empty) {
+//           // sign up here
+//           db.collection("user")
+//             .add({
+//               username: username,
+//               password: password,
+//             })
+//             .then((data) => {
+//               console.log("Sign up successful!");
+//               console.log(data.id);
+//             })
+//             .catch((err) => console.log(err));
+//         } else {
+//           console.log("Username is existed");
+//         }
+//       });
+//   });
+// }
+
+export function signUp(userInformation) {
+  return new Promise((resolve, reject) => {
+    const { username, password } = userInformation;
+    console.log(userInformation);
+    db.collection("user")
+      .where("username", "==", username)
+      .get()
+      .then((querySnapshot) => {
+        if (!querySnapshot.empty) {
+          throw new Error("Username was existed!");
+        }
+      })
+      .then(() => {
+        db.collection("user")
+          .add({
+            username: username,
+            password: password,
+          })
+          .then((data) => {
+            console.log("Sign up successful!");
+            console.log(data.id);
+          });
+      });
+  });
 }
